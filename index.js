cd3b3773c570f925b0a63d859b8dd5f56a84c4f6
theGouge <thegouge42@gmail.com>
2018-06-10 13:58:29 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Instructions Go Here
+
+export function caesarCipher(str) {
+  // Code Goes Here
+}
+
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+
+});
---
+++ index.test.js
@@ -0,0 +1,13 @@
+import {} from "./index.js";
+
+
+beforeEach(() => {
+
+});
+
+describe("Palindrome Checker", () => {
+  it("Should return the right thingy", () => {
+    expect(typeof output).toBe("");
+  });
+
+});
