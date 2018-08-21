62fd0256c99fabfd1b95e93edf0957bae2b8702e
theGouge <thegouge42@gmail.com>
2018-08-21 12:43:12 -0700

    added first solution

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // Instructions Go Here
 
 export function telephoneCheck(str) {
-  // Code Goes Here
+  let testStr = str;
+  let numOnly = str.replace(/[\s-\x28\x29]/g, "");
+
+  if (str.length < 10 || /[^\d\x28\x29-\s]/.test(str)) {
+    return false;
+  }
+
+  if (/\x28/.test(testStr)) {
+    testStr = testStr.replace(/\x28(?=\d{3}\x29)|\x29/g, "");
+  }
+
+  if (numOnly.length > 10) {
+    if (/^1/.test(numOnly)) {
+      testStr = testStr.replace(/^1(?=\s|\x28)\s?/, "");
+      numOnly = numOnly.replace(/^1/, "");
+      if (numOnly.length > 10) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  if (/(\d{3})(\s|-)?(\d{3})(\s|-)?(\d{4})/.test(testStr)) {
+    if (/(\x28|\x29)|(^-)/.test(testStr)) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+
 }
 
 const input = document.getElementById("input");
